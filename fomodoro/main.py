fe641c3b193750d7b9928057090f02474dbd155e
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-04-21 01:50:51 -0400

    Make stopwatch ui

--- fomodoro/main.py
+++ fomodoro/main.py
@@ -1,1 +1,54 @@
+import curses
+from curses import wrapper
+from time import sleep, strftime, gmtime
+from json import load, dump
 
+from fomodoro.utils import States, SECONDS_FORMAT, INFO_FILE
+from fomodoro.stopwatch import Stopwatch
+
+
+stopwatch_obj = Stopwatch()
+
+def main(stdscr):
+    """"""
+    with open(INFO_FILE, 'r', encoding='utf-8') as info_file:
+        info = load(info_file)
+
+    if info["stopwatch_state"] == 'Pause' or info["stopwatch_state"] == " ":
+        stopwatch_obj.start()
+        stopwatch_obj.elapsed_seconds = info["elapsed_seconds"]
+        time_window = curses.newwin(1, 10, 12, 55)
+
+        while stopwatch_obj.state is States.START:
+            sleep(1.0)
+            stopwatch_obj.elapsed_seconds += 1
+            stopwatch_obj.seconds += 1
+            struct_time = gmtime(float(stopwatch_obj.elapsed_seconds))
+            formated_seconds = strftime(SECONDS_FORMAT, struct_time)
+
+            time_window.nodelay(True)
+            time_window.clear()
+            time_window.addstr(formated_seconds[0:], curses.A_BOLD)
+            time_window.refresh()
+            try:
+                character = time_window.getkey()
+                if character == "p":
+                    stopwatch_obj.state = States.PAUSE
+                    info["stopwatch_state"] = 'Pause'
+                    info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch_obj.seconds
+                    with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
+                        dump(info, info_file, indent=2)
+                elif character == "s":
+                    stopwatch_obj.state = States.STOP
+                    info["stopwatch_state"] = 'Stop'
+                    info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch_obj.seconds
+                    with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
+                        dump(info, info_file, indent=2)
+            except curses.error:
+                character = None
+    else:
+        #TODO: Timer
+        pass
+
+
+wrapper(main)
--- fomodoro/utils.py
+++ fomodoro/utils.py
@@ -1,55 +1,31 @@
 """Utilities."""
 from enum import Enum
 from time import sleep
+from os.path import dirname, abspath, join
 
 
 class States(Enum):
     """States of the stopwatch and the timer."""
-    WITHOUT_START: bool = True
-    START: bool = True
-    PAUSE: bool = True
-    RESTART: bool = True
-    STOP: bool = True
+    WITHOUT_START: str = 'Without start'
+    START: str = 'Start'
+    PAUSE: str = 'Pause'
+    STOP: str = 'Stop'
 
-# STOPWATCH
-class Stopwatch():
-    """To can manipulate the stopwatch easiest by functions."""
-    def __init__(self) -> None:
-        self.state = States.WITHOUT_START
-        self.elapsed_seconds = 0
+SECONDS_FORMAT = '%M:%S'
 
-stopwatch_obj = Stopwatch()
+INFO_FILE = join(dirname(abspath(__file__)), 'info.json')
 
-
-def increase(stopwatch: Stopwatch) -> None:
-    """To increase amount of elapsed seconds."""
-    while stopwatch.state == States.START or stopwatch.state == States.RESTART:
-        sleep(1.0)
-        stopwatch.elapsed_seconds += 1
+#def calculate_amount_of_seconds_for_the_timer(stopwatch: Stopwatch):
+#    """To calculate the amount of seconds for the timer."""
+#    amount_of_seconds_for_the_timer: int = (stopwatch.elapsed_seconds / 5) * 60
+#
+#    return amount_of_seconds_for_the_timer
 
 
-
-def calculate_amount_of_seconds_for_the_timer(stopwatch: Stopwatch):
-    """To calculate the amount of seconds for the timer."""
-    amount_of_seconds_for_the_timer: int = (stopwatch.elapsed_seconds / 5) * 60
-
-    return amount_of_seconds_for_the_timer
-
-
-# TIMER
-class Timer():
-    """To can manipulate the timer easiest by functions."""
-    def __init__(self) -> None:
-        self.state = States.WITHOUT_START
-        self.break_time_in_seconds = 0
-
-timer_obj = Timer()
-
-
-def decrease(break_time_in_seconds: int, timer: Timer):
-    """To decrease amount of break time seconds."""
-    timer.break_time_in_seconds = break_time_in_seconds
-
-    while timer.state == States.START or timer.state == States.RESTART:
-        sleep(1.0)
-        timer.break_time_in_seconds -= 1
+#def decrease(break_time_in_seconds: int, timer: Timer):
+#    """To decrease amount of break time seconds."""
+#    timer.break_time_in_seconds = break_time_in_seconds
+#
+#    while timer.state == States.START or timer.state == States.RESTART:
+#        sleep(1.0)
+#        timer.break_time_in_seconds -= 1
