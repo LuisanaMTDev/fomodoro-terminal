ade29d9a4016f3cc04bc95e3ae099039336e0b05
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-04-22 14:55:29 -0400

    Add code to play the bell sound when the timer finished

--- fomodoro/utils.py
+++ fomodoro/utils.py
@@ -7,6 +7,8 @@ TIME_FORMAT = '%H:%M:%S'
 
 INFO_FILE = join(dirname(abspath(__file__)), 'info.json')
 
+BELL_SOUND_FILE = join(dirname(abspath(__file__)), 'bell.wav')
+
 class States(Enum):
     """States of the stopwatch and the timer."""
     WITHOUT_START: str = 'Without start'
