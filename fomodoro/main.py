3916d9d463da13ed81713a9e561113a8ebde4d70
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-04-22 00:06:22 -0400

    Fix KeyboardInterrupt exception when control + c is pressed

--- fomodoro/main.py
+++ fomodoro/main.py
@@ -30,33 +30,36 @@ def main(stdscr):
         stdscr.addstr(5, 3, "- Press s to stop the stopwatch.")
         stdscr.refresh()
 
-        while stopwatch_obj.state is States.START:
-            sleep(1.0)
-            stopwatch_obj.elapsed_seconds += 1
-            stopwatch_obj.seconds += 1
-            struct_time_stopwatch = gmtime(float(stopwatch_obj.elapsed_seconds))
-            stopwatch_formated_seconds = strftime(SECONDS_FORMAT, struct_time_stopwatch)
+        try:
+            while stopwatch_obj.state is States.START:
+                sleep(1.0)
+                stopwatch_obj.elapsed_seconds += 1
+                stopwatch_obj.seconds += 1
+                struct_time_stopwatch = gmtime(float(stopwatch_obj.elapsed_seconds))
+                stopwatch_formated_seconds = strftime(SECONDS_FORMAT, struct_time_stopwatch)
 
-            time_window.nodelay(True)
-            time_window.clear()
-            time_window.addstr(stopwatch_formated_seconds[0:], curses.A_BOLD)
-            time_window.refresh()
-            try:
-                stopwatch_character = time_window.getkey()
-                if stopwatch_character == "p":
-                    stopwatch_obj.pause()
-                    info["stopwatch_state"] = 'Pause'
-                    info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch_obj.seconds
-                    with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
-                        dump(info, info_file, indent=2)
-                elif stopwatch_character == "s":
-                    stopwatch_obj.stop()
-                    info["stopwatch_state"] = 'Stop'
-                    info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch_obj.seconds
-                    with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
-                        dump(info, info_file, indent=2)
-            except curses.error:
-                stopwatch_character = None
+                time_window.nodelay(True)
+                time_window.clear()
+                time_window.addstr(stopwatch_formated_seconds[0:], curses.A_BOLD)
+                time_window.refresh()
+                try:
+                    stopwatch_character = time_window.getkey()
+                    if stopwatch_character == "p":
+                        stopwatch_obj.pause()
+                        info["stopwatch_state"] = 'Pause'
+                        info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch_obj.seconds
+                        with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
+                            dump(info, info_file, indent=2)
+                    elif stopwatch_character == "s":
+                        stopwatch_obj.stop()
+                        info["stopwatch_state"] = 'Stop'
+                        info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch_obj.seconds
+                        with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
+                            dump(info, info_file, indent=2)
+                except curses.error:
+                    stopwatch_character = None
+        except KeyboardInterrupt:
+            pass
     elif info["stopwatch_state"] == "Stop":
         stdscr.clear()
         stdscr.addstr(3, 7, "Instructions:\n")
@@ -72,37 +75,40 @@ def main(stdscr):
         timer_obj.break_time_in_seconds = int(amount_of_seconds_for_the_timer)
         timer_obj.start()
 
-        while timer_obj.state is States.START:
-            sleep(1.0)
-            if timer_obj.break_time_in_seconds > 0:
-                timer_obj.break_time_in_seconds -= 1
-            else:
-                timer_obj.stop()
-                playsound("bell.mp3")
-                info["timer_state"] = " "
-                info["leftover_break_time_in_seconds"] = 0
-                info["elapsed_seconds"] = 0
-                info["stopwatch_state"] = " "
-            
-            struct_time_timer = gmtime(timer_obj.break_time_in_seconds)
-            timer_formated_seconds = strftime(SECONDS_FORMAT, struct_time_timer)
+        try:
+            while timer_obj.state is States.START:
+                sleep(1.0)
+                if timer_obj.break_time_in_seconds > 0:
+                    timer_obj.break_time_in_seconds -= 1
+                else:
+                    timer_obj.stop()
+                    playsound("bell.mp3")
+                    info["timer_state"] = " "
+                    info["leftover_break_time_in_seconds"] = 0
+                    info["elapsed_seconds"] = 0
+                    info["stopwatch_state"] = " "
+                
+                struct_time_timer = gmtime(timer_obj.break_time_in_seconds)
+                timer_formated_seconds = strftime(SECONDS_FORMAT, struct_time_timer)
 
-            time_window.nodelay(True)
-            time_window.clear()
-            time_window.addstr(timer_formated_seconds[0:], curses.A_BOLD)
-            time_window.refresh()
-            try:
-                timer_character = time_window.getkey()
-                if timer_character == "p":
-                    timer_obj.pause()
-                    info["timer_state"] = "Pause"
-                    info["leftover_break_time_in_seconds"] = timer_obj.break_time_in_seconds
-                    with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
-                        dump(info, info_file, indent=2)
-            except curses.error:
-                timer_character = None
+                time_window.nodelay(True)
+                time_window.clear()
+                time_window.addstr(timer_formated_seconds[0:], curses.A_BOLD)
+                time_window.refresh()
+                try:
+                    timer_character = time_window.getkey()
+                    if timer_character == "p":
+                        timer_obj.pause()
+                        info["timer_state"] = "Pause"
+                        info["leftover_break_time_in_seconds"] = timer_obj.break_time_in_seconds
+                        with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
+                            dump(info, info_file, indent=2)
+                except curses.error:
+                    timer_character = None
+        except KeyboardInterrupt:
+            pass
             
-            with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
-                dump(info, info_file, indent=2)
+        with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
+            dump(info, info_file, indent=2)
 
 fomodoro = wrapper(main)
