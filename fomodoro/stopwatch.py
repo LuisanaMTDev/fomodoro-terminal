e723e0828bdf46f8ad741a55d7b095b721be39f8
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-05-08 18:15:14 -0400

    Redactor code

--- fomodoro/stopwatch.py
+++ fomodoro/stopwatch.py
@@ -1,18 +1,22 @@
-"""This module has all code related to stopwatch."""
+"""
+This module has all code related to stopwatch.
+"""
 from json import load, dump
-from time import sleep, gmtime, strftime
+from time import sleep
 import curses
 
-from fomodoro.utils import States, INFO_FILE, TIME_FORMAT
+from fomodoro.utils import States, INFO_FILE, format_seconds
 from fomodoro.stats import add_stopwatch_record
 
 
 class Stopwatch():
-    """To can manipulate the stopwatch easiest by methods."""
+    """
+    To can manipulate the stopwatch easiest by methods.
+    """
     def __init__(self) -> None:
         self.state = States.WITHOUT_START
-        self.elapsed_seconds = 0
-        self.seconds = 0
+        self.elapsed_seconds: int = 0
+        self.seconds: int = 0
 
     def start(self):
         self.state = States.START
@@ -26,10 +30,13 @@ class Stopwatch():
 
 stopwatch_obj = Stopwatch()
 
+
 def prepare_stopwatch() -> Stopwatch:
-    """This function will prepare the stopwatch obj to run stopwatch."""
+    """
+    This function will prepare the stopwatch obj to run stopwatch.
+    """
     with open(INFO_FILE, 'r', encoding='utf-8') as info_file:
-        info = load(info_file)
+        info: dict = load(info_file)
 
     stopwatch_obj.start()
     stopwatch_obj.elapsed_seconds = info["elapsed_seconds"]
@@ -38,35 +45,36 @@ def prepare_stopwatch() -> Stopwatch:
 
 
 def run_stopwatch(stopwatch: Stopwatch):
-    """This function run stopwatch."""
+    """
+    This function run stopwatch.
+    """
     time_window = curses.newwin(1, 9, 1, 16)
 
     with open(INFO_FILE, 'r', encoding='utf-8') as info_file:
-        info = load(info_file)
+        info: dict = load(info_file)
 
     while stopwatch.state is States.START:
         sleep(1.0)
         stopwatch.elapsed_seconds += 1
         stopwatch.seconds += 1
-        struct_time_stopwatch = gmtime(float(stopwatch.elapsed_seconds))
-        stopwatch_formated_seconds = strftime(TIME_FORMAT, struct_time_stopwatch)
 
         time_window.nodelay(True)
         time_window.clear()
-        time_window.addstr(stopwatch_formated_seconds[0:], curses.A_BOLD)
+        time_window.addstr(format_seconds(stopwatch.elapsed_seconds)[0:],
+                           curses.A_BOLD)
         time_window.refresh()
         try:
             stopwatch_character = time_window.getkey()
             if stopwatch_character == "p":
                 stopwatch.pause()
-                info["stopwatch_state"] = States.PAUSE.value
-                info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch.seconds
+                info.update({"stopwatch_state": States.PAUSE.value})
+                info.update({"elapsed_seconds": info["elapsed_seconds"] + stopwatch.seconds})
                 with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
                     dump(info, info_file, indent=2)
             elif stopwatch_character == "s":
                 stopwatch.stop()
-                info["stopwatch_state"] = States.STOP.value
-                info["elapsed_seconds"] = info["elapsed_seconds"] + stopwatch.seconds
+                info.update({"stopwatch_state": States.STOP.value})
+                info.update({"elapsed_seconds": info["elapsed_seconds"] + stopwatch.seconds})
                 add_stopwatch_record(info["elapsed_seconds"])
                 with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
                     dump(info, info_file, indent=2)
--- fomodoro/timer.py
+++ fomodoro/timer.py
@@ -1,18 +1,22 @@
-"""This module has all code related to timer."""
+"""
+This module has all code related to timer.
+"""
 from json import load, dump
-from time import sleep, gmtime, strftime
+from time import sleep
 from winsound import PlaySound, SND_FILENAME
 import curses
 
-from fomodoro.utils import States, INFO_FILE, BELL_SOUND_FILE, TIME_FORMAT
+from fomodoro.utils import States, INFO_FILE, BELL_SOUND_FILE, format_seconds
 from fomodoro.stats import add_timer_record
 
 
 class Timer():
-    """To can manipulate the timer easiest by functions."""
+    """
+    To can manipulate the timer easiest by functions.
+    """
     def __init__(self) -> None:
         self.state = States.WITHOUT_START
-        self.break_time_in_seconds = 0
+        self.break_time_in_seconds: int = 0
 
     def start(self):
         self.state = States.START
@@ -23,12 +27,16 @@ class Timer():
     def stop(self):
         self.state = States.STOP
 
+
 timer_obj = Timer()
 
+
 def prepare_timer() -> Timer:
-    """This function prepare the timer obj to run timer."""
+    """
+    This function prepare the timer obj to run timer.
+    """
     with open(INFO_FILE, 'r', encoding='utf-8') as info_file:
-        info = load(info_file)
+        info: dict = load(info_file)
 
     timer_obj.start()
     if info["timer_state"] == States.WITHOUT_START.value:
@@ -43,11 +51,13 @@ def prepare_timer() -> Timer:
 
 
 def run_timer(timer: Timer):
-    """This function run timer."""
+    """
+    This function run timer.
+    """
     time_window = curses.newwin(1, 9, 1, 16)
 
     with open(INFO_FILE, 'r', encoding='utf-8') as info_file:
-        info = load(info_file)
+        info: dict = load(info_file)
 
     while timer.state is States.START:
         sleep(1.0)
@@ -56,29 +66,27 @@ def run_timer(timer: Timer):
         else:
             timer.stop()
 
-            info["timer_state"] = States.WITHOUT_START.value
-            info["leftover_break_time_in_seconds"] = 0
-            info["elapsed_seconds"] = 0
-            info["stopwatch_state"] = States.WITHOUT_START.value
+            info.update({"timer_state": States.WITHOUT_START.value})
+            info.update({"leftover_break_time_in_seconds": 0})
+            info.update({"elapsed_seconds": 0})
+            info.update({"stopwatch_state": States.WITHOUT_START.value})
             with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
                 dump(info, info_file, indent=2)
 
             PlaySound(BELL_SOUND_FILE, SND_FILENAME)
 
-        struct_time_timer = gmtime(timer.break_time_in_seconds)
-        timer_formated_seconds = strftime(TIME_FORMAT, struct_time_timer)
-
         time_window.nodelay(True)
         time_window.clear()
-        time_window.addstr(timer_formated_seconds[0:], curses.A_BOLD)
+        time_window.addstr(format_seconds(timer.break_time_in_seconds)[0:],
+                           curses.A_BOLD)
         time_window.refresh()
         try:
             timer_character = time_window.getkey()
             if timer_character == "p":
                 timer.pause()
                 
-                info["timer_state"] = States.PAUSE.value
-                info["leftover_break_time_in_seconds"] = timer.break_time_in_seconds
+                info.update({"timer_state": States.PAUSE.value})
+                info.update({"leftover_break_time_in_seconds": timer.break_time_in_seconds})
                 with open(INFO_FILE, 'w', encoding='utf-8') as info_file:
                     dump(info, info_file, indent=2)
         except curses.error:
