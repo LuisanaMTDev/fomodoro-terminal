8ea4bb2fe85a776c37f2b198297c21a3f04976fe
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-05-08 18:14:28 -0400

    Separe code to format seconds in a function

--- fomodoro/utils.py
+++ fomodoro/utils.py
@@ -1,6 +1,9 @@
-"""On this module there are util code."""
+"""
+On this module there are util code.
+"""
 from enum import Enum
 from os.path import dirname, abspath, join
+from time import gmtime, strftime
 
 
 TIME_FORMAT = '%H:%M:%S'
@@ -11,9 +14,19 @@ DATA_BASE_FILE = join(dirname(abspath(__file__)), 'fomodoro_terminal.db')
 
 BELL_SOUND_FILE = join(dirname(abspath(__file__)), 'bell.wav')
 
+
 class States(Enum):
-    """States of the stopwatch and the timer."""
+    """
+    States of the stopwatch and the timer.
+    """
     WITHOUT_START: str = ' '
     START: str = 'Start'
     PAUSE: str = 'Pause'
     STOP: str = 'Stop'
+
+
+def format_seconds(seconds: int) -> str:
+    struct_time = gmtime(float(seconds))
+    formated_seconds = strftime(TIME_FORMAT, struct_time)
+
+    return formated_seconds
