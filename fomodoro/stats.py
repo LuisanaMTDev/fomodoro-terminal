3957c05b4fe370190bc74ca1d3e4cef8832292b8
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-05-08 18:19:11 -0400

    Code function to get sum of seconds on stopwatch and timer
    The sum is done by day, week, month and year.

--- fomodoro/stats.py
+++ fomodoro/stats.py
@@ -1,13 +1,21 @@
-"""This module has all code related to stats feature."""
+"""
+This module has all code related to stats feature.
+"""
 from datetime import datetime
 from sqlite3 import connect, OperationalError, Cursor, Connection
 import click as ck
 
 from fomodoro.utils import DATA_BASE_FILE
 
+
 # IDEA FOR TEST: Print whatever raised exception on code that use sqlite3.
-def create_table(cursor: Cursor, connection: Connection, table_name: str) -> None:
-    """This function create stopwatch and timer tables on fomodoro_terminal database."""
+def create_table(cursor: Cursor,
+                 connection: Connection,
+                 table_name: str) -> None:
+    """
+    This function create stopwatch and timer tables
+    on fomodoro_terminal database.
+    """
     try:
         cursor.execute(
             f"""
@@ -25,21 +33,24 @@ def create_table(cursor: Cursor, connection: Connection, table_name: str) -> Non
 
 
 def add_stopwatch_record(elapsed_seconds: int) -> None:
-    """This function create a new record on stopwatch table."""
+    """
+    This function create a new record on stopwatch table.
+    """
     date = datetime.now().isoformat()[0:10]
+    week = datetime.now().strftime('%W')
 
     connection = connect(DATA_BASE_FILE)
     cursor = connection.cursor()
 
     create_table(cursor, connection, "stopwatch")
 
-    #IDEA FOR TEST: After execute this function execute a select query
-    #to compare result data with insert insert data.
+    # IDEA FOR TEST: After execute this function execute a select query
+    # to compare result data with insert insert data.
     cursor.execute(
         """
         INSERT INTO stopwatch(seconds, date) VALUES (?, ?)
         """,
-        (elapsed_seconds, date)
+        (elapsed_seconds, f"{date},{week}")
     )
     connection.commit()
 
@@ -47,32 +58,109 @@ def add_stopwatch_record(elapsed_seconds: int) -> None:
 
 
 def add_timer_record(amount_of_seconds_for_the_timer: int) -> None:
-    """This function create a new record on timer table."""
+    """
+    This function create a new record on timer table.
+    """
     date = datetime.now().isoformat()[0:10]
+    week = datetime.now().strftime('%W')
 
     connection = connect(DATA_BASE_FILE)
     cursor = connection.cursor()
 
     create_table(cursor, connection, "timer")
 
-    #IDEA FOR TEST: After execute this function execute a select query
-    #to compare result data with insert insert data.
+    # IDEA FOR TEST: After execute this function execute a select query
+    # to compare result data with insert insert data.
     cursor.execute(
         """
         INSERT INTO timer(seconds, date) VALUES (?, ?)
         """,
-        (amount_of_seconds_for_the_timer, date)
+        (amount_of_seconds_for_the_timer, f"{date},{week}")
     )
     connection.commit()
 
     connection.close()
 
 
-def get_stats() -> None:
-    """This function get stopwatch and timer stats."""
+def get_seconds() -> list[tuple[tuple, tuple, tuple, tuple]]:
+    """
+    This function get stopwatch and timer seconds
+    associated with whatever day, week, month, year.
+    """
+    day = datetime.now().strftime('%d')
+    week = datetime.now().strftime('%W')
+    month = datetime.now().strftime('%m')
+    year = datetime.now().strftime('%Y')
+
+    connection = connect(DATA_BASE_FILE)
+    cursor = connection.cursor()
+
+    sum_of_the_stopwatch_seconds_associated_with_day = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM stopwatch
+        WHERE date LIKE '{year}-{month}-{day},%'
+        """
+    ).fetchone()
+    sum_of_the_stopwatch_seconds_associated_with_week = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM stopwatch
+        WHERE date LIKE '{year}%,{week}'
+        """
+    ).fetchone()
+    sum_of_the_stopwatch_seconds_associated_with_month = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM stopwatch
+        WHERE date LIKE '{year}-{month}-__%'
+        """
+    ).fetchone()
+    sum_of_the_stopwatch_seconds_associated_with_year = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM stopwatch
+        WHERE date LIKE '{year}%'
+        """
+    ).fetchone()
+
+    sum_of_the_timer_seconds_associated_with_day = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM timer
+        WHERE date LIKE '{year}-{month}-{day},%'
+        """
+    ).fetchone()
+    sum_of_the_timer_seconds_associated_with_week = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM timer
+        WHERE date LIKE '{year}%,{week}'
+        """
+    ).fetchone()
+    sum_of_the_timer_seconds_associated_with_month = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM timer
+        WHERE date LIKE '{year}-{month}-__%'
+        """
+    ).fetchone()
+    sum_of_the_timer_seconds_associated_with_year = cursor.execute(
+        f"""
+        SELECT sum(seconds) FROM timer
+        WHERE date LIKE '{year}%'
+        """
+    ).fetchone()
+    connection.close()
+
+    return [
+        (sum_of_the_stopwatch_seconds_associated_with_day,
+         sum_of_the_stopwatch_seconds_associated_with_week,
+         sum_of_the_stopwatch_seconds_associated_with_month,
+         sum_of_the_stopwatch_seconds_associated_with_year),
+        (sum_of_the_timer_seconds_associated_with_day,
+         sum_of_the_timer_seconds_associated_with_week,
+         sum_of_the_timer_seconds_associated_with_month,
+         sum_of_the_timer_seconds_associated_with_year)
+        ]
 
 
 @ck.command
 def show_stats():
-    """This command show stopwatch and timer stats for user."""
+    """
+    This command show stopwatch and timer stats for user.
+    """
     ck.echo("Hello world!")
